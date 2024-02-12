9c2bf38996df04afc03b46f00194c634557d8c1f
Charles <carlos.sigua@gmail.com>
2024-02-12 15:08:05 -0500

    sincronizado

--- cli3/main.go
+++ cli3/main.go
@@ -1,28 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
-
-	"github.com/fatih/color"
 )
 
 func main() {
-	// Crear instancias de colores
-	red := color.New(color.FgRed).SprintFunc()
-	green := color.New(color.FgGreen).SprintFunc()
-	yellow := color.New(color.FgYellow).SprintFunc()
-	blue := color.New(color.FgBlue).SprintFunc()
-
-	// Imprimir mensajes en diferentes colores
-	fmt.Println("Mensaje en color:", red("rojo"))
-	fmt.Println("Mensaje en color:", green("verde"))
-	fmt.Println("Mensaje en color:", yellow("amarillo"))
-	fmt.Println("Mensaje en color:", blue("azul"))
-
-	// Puedes combinar colores
-	fmt.Println("Mensaje en color combinado:", red("rojo"), blue("azul"))
-
-	// Salir con estado de éxito
-	os.Exit(0)
+	name := flag.String("name", "world", "The name to greet.")
+	flag.Parse()
+	fmt.Printf("Hello, %s!\n", *name)
 }
--- hello/go.mod
+++ hello/go.mod
@@ -1,3 +1,8 @@
-module hello
+module hello/main
 
-go 1.21.2
+go 1.21.6
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/maxence-charriere/go-app/v9 v9.8.0 // indirect
+)
