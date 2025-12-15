0c71f3344ee9961a152754964ce852fc83942456
winadminos <carlos.sigua@gmail.com>
2025-12-14 23:52:51 -0500

    (bingofin): add main skeleton with --version flag

--- apps/bingofin/cmd/bingofin/main.go
+++ apps/bingofin/cmd/bingofin/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/csidevrik/bingofin/internal/app"
+)
+
+var (
+	version = "1.0.0"
+	commit  = "iniciando"
+	date    = "2024-12-14"
+)
 
 func main() {
-	fmt.Println("bingofin starting...")
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "--version", "-v", "version":
+			fmt.Printf("bingofin %s (commit=%s, date=%s)\n", version, commit, date)
+			return
+		case "--help", "-h", "help":
+			fmt.Println("Usage:")
+			fmt.Println("  bingofin --version")
+			fmt.Println("  bingofin")
+			return
+		}
+		fmt.Println("bingofin starting...")
+	}
+	app.Run()
 }
---
+++ apps/bingofin/internal/app/app.go
@@ -0,0 +1,8 @@
+package app
+
+import "fmt"
+
+func Run() {
+	fmt.Println("Hello from bingofin 👋")
+	fmt.Println("Try: bingofin --version")
+}
